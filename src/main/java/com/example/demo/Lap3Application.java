e12cfeaded5cbfe7be700f414fdfcfa32cd1675f
Quynh <diemthidiemquynh@gmail.com>
2023-05-21 12:47:46 +0700

    first commit

---
+++ src/main/java/com/example/demo/Lap3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lap3Application.class, args);
+	}
+
+}
